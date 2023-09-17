1602f7a3b89ba843ebac147107e46a60df9611c1
ramakanth98 <ramakanth1998@gmail.com>
2023-09-17 15:18:32 -0400

    Added user functionalities and added user.py and post.py

--- app/main.py
+++ app/main.py
@@ -1,23 +1,20 @@
-from argparse import OPTIONAL
-from fastapi import FastAPI, Response, status, HTTPException
+from typing import List
+from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body, Depends
 from pydantic import BaseModel
 from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
-from . import models
+from . import models, schemas, utils
 from .database import engine, get_db
 from sqlalchemy.orm import Session
+from .routers import post, user
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
 
 while True:
     try:
@@ -42,67 +39,13 @@ def find_index_post(id):
         if p['id'] == id:
             return i
 
+
+app.include_router(post.router)
+app.include_router(user.router)
+
 #ALL API's
 @app.get("/")
 def root():
     return {"message": "Reload check 222"}
 
-@app.get("/posts")
-def get_posts(db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM posts""")
-    # posts = cursor.fetchall()
-    # print(posts)
-    posts = db.query(models.Post).all()
-    return {"data": posts}
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post:Post, db: Session = Depends(get_db)):
-    # cursor.execute(""" INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, (post.title, post.content, post.published))
-    # new_post = cursor.fetchone()
-    # conn.commit()
-    #print(**post)
-    new_post = models.Post(**post.dict())
-    #Commit to database
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-    return{"data": new_post}
-
-@app.get('/posts/{id}')
-def get_post(id: int, db: Session = Depends(get_db)):
-    # cursor.execute(""" SELECT * FROM posts WHERE id=%s """, (str(id),))
-    # post = cursor.fetchone()
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
-    return {"data": post}
-
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
-    # cursor.execute("""DELETE FROM posts WHERE id=%s RETURNING *""", (str(id),))
-    # deleted_post = cursor.fetchone()
-    # conn.commit()
-    post = db.query(models.Post).filter(models.Post.id == id)
-    if post.first()==None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} does not exist")    
-    post.delete(synchronize_session=False)
-    db.commit()
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-@app.put("/posts/{id}")
-def update_post(id: int, updated_post: Post,  db: Session = Depends(get_db)):
-    # cursor.execute("""UPDATE posts SET title=%s, content=%s, published=%s WHERE id=%s RETURNING *""", (post.title, post.content, post.published, str(id)))
-    # updated_post = cursor.fetchone()
-    # conn.commit()
-    post_query = db.query(models.Post).filter(models.Post.id == id)
-    post = post_query.first()
-    if post==None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} does not exist")
-    post_query.update(updated_post.dict(), synchronize_session=False)
-    db.commit()
-    return {'data': post_query.first()}
-
-@app.get("/sqlalchemy")
-def test_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    return {"data": posts}
--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
 
-
+#SQLAlchemy model
 class Post(Base):
     __tablename__ = "posts"
 
@@ -11,4 +11,12 @@ class Post(Base):
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)    
     published = Column(Boolean, server_default='TRUE', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, nullable=True)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
